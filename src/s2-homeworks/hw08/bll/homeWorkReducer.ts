bed2e20e7f099fb23ad5010666e5ded41f941335
AlexandrLatushko <latushkoalex94@gmail.com>
2024-10-23 12:17:07 +0300

    add HW9

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,23 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+    switch (action.type)  {
         case 'sort': { // by name
-
-            return state // need to fix
+            if (action.payload === 'up') {
+                return state.slice().sort((a, b) => a.name.localeCompare(b.name)); // sort ascending
+            }
+            if (action.payload === 'down') {
+                return state.slice().sort((a, b) => b.name.localeCompare(a.name)); // sort descending
+            }
+            break;
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload); // filter by age
         }
         default:
-            return state
+            return state;
     }
-}
+    return state; // need to fix
+    }
+
